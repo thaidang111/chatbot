d43098579b1dc8db26fc671d49a17386dab078da
thaidang111 <88382936+thaidang111@users.noreply.github.com>
2023-02-06 17:11:41 +0700

    Update Constants.py

--- Chatbot/Constants.py
+++ Chatbot/Constants.py
@@ -1,8 +1,8 @@
 import openai
 
 # OpenAI-API (get on OpenAI - must be of an USA account)
-openai.api_key = 'HIDDEN'
+openai.api_key = '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'
 
 # Token for fanpage and verify it
-_PAGE_ACCESS_TOKEN = 'HIDDEN'
+_PAGE_ACCESS_TOKEN = 'EAASEt3mSVBoBAHaXEVStzoNLN0siFZCI0FNZCUzcLf2nzkSqlS9EnL3Qu5P2VvxcPwwk4u1WiHyEuYzHqxrcFDW0FNqkcztLddby6Yh1RG9zJIMhDPSxm95zpc3DrxyLJkzq72dNnKOBTResvLjBiFNi2kmOdTCvJlCz8oAbCD92sG14nblau4ID3LJOgoEJANqsrZAewZDZD'
 _VERIFY_TOKEN = 'HIDDEN'
